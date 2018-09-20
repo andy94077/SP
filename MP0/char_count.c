a52fdc79a5666dd6710c43a71f7739c513dd05d7
andy94077 <andy94077@gmail.com>
2018-09-20 14:53:36 +0800

    finished

---
+++ MP0/char_count.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(int argc, char const *argv[])
+{
+	FILE *f;
+	const char *charset = argv[1];
+	if(argc==2)
+		f=stdin;
+	else
+	{
+		f = fopen(argv[2], "r");
+		if(f==NULL)
+		{
+			printf("error\n");
+			exit(8);
+		}
+	}
+
+	unsigned long long table[128] = {0};
+	int c;
+	while((c=getc(f))!=EOF)
+	{
+		if(c=='\n')
+		{
+			unsigned long long sum = 0;
+			for (int i = 0; charset[i] != '\0';i++)
+				sum += table[charset[i]];
+			printf("%llu\n", sum);
+			memset(table, 0, sizeof(table));
+		}
+		else
+			table[c]++;
+	}
+	
+	unsigned long long sum = 0;
+	for (int i = 0; charset[i] != '\0';i++)
+		sum += table[charset[i]];
+	printf("%llu\n", sum);
+	return 0;
+}
