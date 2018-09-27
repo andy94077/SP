60c14a64e16f1919083dccc041416e85084dac60
andy94077 <andy94077@gmail.com>
2018-09-27 17:20:40 +0800

    maybe faster

--- MP0/char_count.c
+++ MP0/char_count.c
@@ -4,33 +4,32 @@
 int main(int argc, char const *argv[])
 {
 	FILE *f;
-	const char *const charset = argv[1];
-	if(argc==2)
-		f=stdin;
+	int charset[256]={0};
+	for (int i = 0; argv[1][i] != '\0';i++)
+		charset[argv[1][i]]=1;
+	
+	if (argc == 2)
+		f = stdin;
 	else
 	{
 		f = fopen(argv[2], "r");
-		if(f==NULL)
+		if (f == NULL)
 		{
-			fprintf(stderr,"error\n");
+			fprintf(stderr, "error\n");
 			return 1;
 		}
 	}
 
-	int table[256] = {0};
-	int c;
+	int c,sum=0;
 	while((c=fgetc(f))!=EOF)
 	{
 		if(c=='\n')
 		{
-			int sum = 0;
-			for (int i = 0; charset[i] != '\0';i++)
-				sum += table[charset[i]];
 			printf("%d\n", sum);
-			memset(table, 0, sizeof(table));
+			sum = 0;
 		}
-		else
-			table[c]++;
+		else if(charset[c])
+			sum++;
 	}
 	
 	return 0;
