5b53ba5168b2024c7a59a459e7a677ab5e6882fe
andy94077 <andy94077@gmail.com>
2018-10-27 17:35:07 +0800

    partial progress

--- MP1/loser.cpp
+++ MP1/loser.cpp
@@ -9,9 +9,16 @@
 #include <dirent.h>
 using namespace std;
 constexpr int PREFIX = 24;
-inline vector<string>&& load_filename(const char dir[])
+
+template <class T>
+inline void seek_and_read(FILE *f, int offset, int whence, T& buf)
 {
-	vector<string> filename;
+	fseek(f, offset, whence);
+	fread(&buf, sizeof(buf), 1, f);
+}
+
+inline void load_filename(const char dir[], vector<string>& filename)
+{
 	DIR *d = opendir(dir);
 	dirent *f;
 	//read files in dir
@@ -20,21 +27,20 @@ inline vector<string>&& load_filename(const char dir[])
 
 	//sort files by name
 	sort(filename.begin(), filename.end());
-	return move(filename);
 }
-void jumptolast(FILE *f, deque<int> &commit_log, int n = 1)
+void get_last_n_commit_pos(FILE *f, deque<long> &commit_log, int n = 1)
 {
 	fseek(f, 0, SEEK_END);
-	int end_pos = ftell(f);
+	long end_pos = ftell(f);
 
 	fseek(f, PREFIX, SEEK_SET);
 	commit_log.push_back(ftell(f));
 
 	uint32_t commit_size;fread(&commit_size, sizeof(commit_size), 1, f);
-	while (commit_log.back() + commit_size < end_pos)
+	while ((long long)commit_log.back() + (long long)commit_size < (long long)end_pos)
 	{
 		fseek(f, commit_size+PREFIX, SEEK_CUR);
-		if (commit_log.size() >= n)
+		if (commit_log.size() >= (size_t)n)
 			commit_log.pop_front();
 		commit_log.push_back(ftell(f));
 		fread(&commit_size, sizeof(commit_size), 1, f);
@@ -42,7 +48,7 @@ void jumptolast(FILE *f, deque<int> &commit_log, int n = 1)
 }
 void status(const char dir[])
 {
-	vector<string> filename = load_filename(dir);
+	vector<string> filename; load_filename(dir,filename);
 
 	FILE *los = fopen(".loser_record", "rb");
 	if (los == NULL)
@@ -55,23 +61,25 @@ void status(const char dir[])
 	}
 	else
 	{
-		deque<int> last_commit;
-		jumptolast(los, last_commit);
-		fseek(los, last_commit[0]-20, SEEK_SET);
+		deque<long> commit_pos;
+		get_last_n_commit_pos(los, commit_pos);
+		
 		//add, modify, copy, delete
-		uint32_t amcd_n;fread(&amcd_n, sizeof(amcd_n), 1, los);
+		uint32_t amcd_n;
+		seek_and_read(los, commit_pos[0] - 20, SEEK_SET, amcd_n);
 
 		//jump to md5 part
-		fseek(los, last_commit[0], SEEK_SET);
-		uint32_t commit_size;fread(&commit_size, sizeof(commit_size), 1, los);
-		for(uint32_t i = 0; i < amcd_n; i++)
+		uint32_t commit_size;
+		seek_and_read(los, commit_pos[0], SEEK_SET, commit_size);
+		for (uint32_t i = 0; i < amcd_n; i++)
 		{
-			uint8_t filename_len;fread(&filename_len, sizeof(filename_len), 1, los);
+			uint8_t filename_len;
+			fread(&filename_len, sizeof(filename_len), 1, los);
 			fseek(los, filename_len, SEEK_CUR);
 		}
 
 		//compare files
-		while(ftell(los)<last_commit[0] + commit_size)
+		while(ftell(los)<commit_pos[0] + commit_size)
 		{
 			uint8_t filename_len; fread(&filename_len, sizeof(filename_len), 1, los);
 			char filename[256];	fread(filename, sizeof(char), sizeof(filename), los);
@@ -88,12 +96,16 @@ void log(int n, const char dir[])
 }
 int main(int argc, char const *argv[])
 {
-	if(strcmp(argv[1],"status")==0)
+	/*if(strcmp(argv[1],"status")==0)
 		status(argv[2]);
 	else if(strcmp(argv[1],"commit")==0)
 		commit(argv[2]);
 	else
-		log(atoi(argv[2]), argv[3]);
-
+		log(atoi(argv[2]), argv[3]);*/
+	FILE *f=fopen("test","rb");
+	int a;
+	fseek(f, 0, SEEK_END);
+	seek_and_read(f, 0, SEEK_SET, a);
+	printf("%x\n", a);
 	return 0;
 }
