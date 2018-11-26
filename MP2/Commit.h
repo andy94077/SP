9e5e2bfdddf249ed983691fb77e866049d0c87c1
andy94077 <andy94077@gmail.com>
2018-11-27 00:31:43 +0800

    commit.h not finished yet

--- MP2/Commit.h
+++ MP2/Commit.h
@@ -1,22 +1,31 @@
 #include "read_utils.h"
 #include "FileMd5.h"
+#include <fstream>
+#include <cstdint>
+#include <limits>
+#include <vector>
+#include <utility>
+using namespace std;
 //save the last n commit pos in 'commit_log'
-inline void get_last_n_commit_pos(const string& user_name, deque<uint32_t> &commit_log, size_t n = 1)
+inline void get_commit_pos(const string& user_name, deque<uint32_t> &commit_log, bool only_last_one=false)
 {
-	FILE *logpos_f = fopen((".loser_pos_" + user_name).c_str(), "rb");
-	commit_log.clear();
-	fseek(logpos_f, 0, SEEK_END);
-	uint32_t end_pos = ftell(logpos_f);
-	uint32_t total_commit_n=end_pos/sizeof(uint32_t);
-	if(total_commit_n < n)
-		fseek(logpos_f, 0, SEEK_SET);
-	else
-		fseek(logpos_f, (total_commit_n-n)*sizeof(uint32_t), SEEK_END);
+	ifstream logpos_f("/tmp/.loser_pos_" + user_name, ios::in|ios::binary);
+	assert(!logpos_f.fail());
 
+	commit_log.clear();
 	uint32_t pos;
-	while (read_int(logpos_f,pos)==1)
+	if(only_last_one)
+	{
+		logpos_f.seekg(-4,ios_base::end);
+		logpos_f.read((char *)&pos,sizeof(pos));
 		commit_log.push_back(pos);
-	
+	}
+	else
+	{
+		while (logpos_f.read((char *)&pos,sizeof(pos)))
+			commit_log.push_back(pos);
+	}
+	logpos_f.close();
 }
 void compare_last(vector<FileMd5>& cur_file, vector<FileMd5>& last_file,
 				 vector<FileMd5 *>& new_file, vector<FileMd5 *>& modified, vector<pair<FileMd5 *, FileMd5 *>>& copied, vector<FileMd5 *>& deleted)
@@ -29,7 +38,7 @@ void compare_last(vector<FileMd5>& cur_file, vector<FileMd5>& last_file,
 		last_file_md5.push_back(&last_file[i]);
 	last_file_md5.shrink_to_fit();
 	sort(last_file_md5.begin(), last_file_md5.end(),
-			[](const FileMd5 *a, const FileMd5 *b) -> bool { return memcmp(a->md5, b->md5, MD5_DIGEST_LENGTH) < 0; });
+			[](const FileMd5 *a, const FileMd5 *b) -> bool { return a->md5 < b->md5; });
 
 	/*
 	*	compare files
@@ -40,7 +49,7 @@ void compare_last(vector<FileMd5>& cur_file, vector<FileMd5>& last_file,
 		int result = last_it->name.compare(cur_it->name);
 		if (result==0)
 		{
-			if(memcmp(last_it->md5, cur_it->md5, MD5_DIGEST_LENGTH)!=0)
+			if(last_it->md5 != cur_it->md5)
 				modified.push_back(&last_it[0]);
 			last_it++, cur_it++;
 		}
@@ -49,8 +58,8 @@ void compare_last(vector<FileMd5>& cur_file, vector<FileMd5>& last_file,
 		else
 		{
 			auto it = lower_bound(last_file_md5.begin(), last_file_md5.end(), cur_it->md5,
-									[](const FileMd5 *a, const unsigned char *key) -> bool { return memcmp(a->md5, key, MD5_DIGEST_LENGTH) < 0; });
-			if (it == last_file_md5.end() || memcmp(it[0]->md5, cur_it->md5, MD5_DIGEST_LENGTH)!=0)
+									[](const FileMd5 *a, const string& key) -> bool { return a->md5 < key; });
+			if (it == last_file_md5.end() || it[0]->md5 != cur_it->md5)
 				new_file.push_back(&cur_it[0]);
 			else
 				copied.push_back(make_pair(*it, &cur_it[0]));
@@ -66,8 +75,8 @@ void compare_last(vector<FileMd5>& cur_file, vector<FileMd5>& last_file,
 	for (; cur_it != cur_file.end();cur_it++)
 	{
 		auto it = lower_bound(last_file_md5.begin(), last_file_md5.end(), cur_it->md5,
-								[](const FileMd5 *a, const unsigned char *key) -> bool { return memcmp(a->md5, key, MD5_DIGEST_LENGTH) < 0; });
-		if (it == last_file_md5.end() || memcmp(it[0]->md5, cur_it->md5, MD5_DIGEST_LENGTH)!=0)
+								[](const FileMd5 *a, const string& key) -> bool { return a->md5 < key; });
+		if (it == last_file_md5.end() || it[0]->md5 != cur_it->md5)
 			new_file.push_back(&cur_it[0]);
 		else
 			copied.push_back(make_pair(*it, &cur_it[0]));
@@ -76,70 +85,105 @@ void compare_last(vector<FileMd5>& cur_file, vector<FileMd5>& last_file,
 
 class Commit
 {
-public:
-	uint32_t commit_i;
-	vector<FileMd5> filemd5;
-	uint32_t timestamp;
-  	Commit(const string& user_name);
+  public:
+  	Commit(const string& log_path);
 	~Commit() { }
-	bool fail() { return _fail; }
-	void write(FILE *log_f);
-private:
-	bool _fail;
+	uint32_t commit_i() throw(const char *) { (not_init) ? throw("Commit not init yet!") : return commit_i; }
+	uint32_t timestamp() throw(const char *) { (not_init) ? throw("Commit not init yet!") : return timestamp; }
+	void write(ifstream& log_f);
+	void push_new(const string &path);
+	void push_modified(const string &path);
+	void push_copied(const string &from_path, const string &to_path);
+	void push_deleted(const string &path);
+
+  private:
+	bool not_init;
+	uint32_t commit_i,timestamp,write_pos;
+	vector<FileMd5> new_file, modified, deleted;
+	vector<pair<FileMd5, FileMd5>> copied;
 };
 
-Commit::Commit(const string& user_name):_fail(false)
+Commit::Commit(ifstream &log_f):_fail(false)
 {
-	FILE *log_f = fopen((".loser_record_" + user_name).c_str(), "rb");
-	if(log_f==NULL)
+	if(log_f.fail())
 	{
 		_fail = true;
 		return;
 	}
-	deque<uint32_t> commit_pos;
-	get_last_n_commit_pos(user_name, commit_pos);
 	
 	/*
 	*	jump to md5 part
 	*/
-	fseek(log_f, commit_pos[0], SEEK_SET);
-	uint32_t file_n, add_n, modified_n, copied_n, deleted_n;
-	read_int(log_f, commit_i, file_n, add_n, modified_n, copied_n, deleted_n);
-	//printf("## ci: %u add_n: %x mo: %x co: %x de: %x fp: %x\n", commit_i, add_n, modified_n, copied_n, deleted_n, ftell(log_f));
-	/*
-	*	jumping
-	*/
-	for (uint32_t i = 0; i < add_n + modified_n; i++)
-	{
-		uint8_t filename_len; read_int(log_f, filename_len);
-		//printf("## fl: %hhx\n", filename_len);
-		fseek(log_f, filename_len, SEEK_CUR);
-	}
-	for (uint32_t i = 0; i < copied_n; i++)
-	{
-		uint8_t filename_len; read_int(log_f, filename_len);
-		//printf("## fl: %hhx\n", filename_len);
-		fseek(log_f, filename_len, SEEK_CUR);
+	uint32_t file_n, last_md5_pos;
+	log_f.seekg(-3*(int)sizeof(uint32_t));
+	read_int(log_f, commit_i, file_n, last_md5_pos);
+	log_f.seekg(last_md5_pos);
 
-		read_int(log_f, filename_len);
-		//printf("## fl: %hhx\n", filename_len);
-		fseek(log_f, filename_len, SEEK_CUR);
-	}
-	for (uint32_t i = 0; i < deleted_n; i++)
-	{
-		uint8_t filename_len; read_int(log_f, filename_len);
-		//printf("## fl: %hhx\n", filename_len);
-		fseek(log_f, filename_len, SEEK_CUR);
-	}
-	
-	//read filename and its md5 into vector
+	uint32_t write_pos = log_f.tellg();
 	for (uint32_t i = 0; i < file_n; i++)
 	{
-		uint8_t filename_len; read_int(log_f, filename_len);
-		char filename[256];	read_str(log_f, filename, filename_len);
-		unsigned char md5[MD5_DIGEST_LENGTH]; fread(md5, sizeof(char), MD5_DIGEST_LENGTH, log_f);
-		filemd5.push_back(FileMd5{filename,md5});
+		string filename, md5;
+		log_f >> filename >> md5;
+		filemd5.push_back(FileMd5{filename, md5});
+	}
+	
+}
+
+void Commit::write(ifstream& log_f)
+{
+
+
+
+	fseek(f,0,SEEK_END);
+	uint32_t commit_st_pos=ftell(f)/sizeof(uint32_t);
+	fwrite(&commit_st_pos, sizeof(uint32_t), 1, pos_f);
+	fclose(pos_f);
+
+	vector<FileMd5 *> new_file, modified, deleted;
+	vector<pair<FileMd5 *, FileMd5 *>> copied;
+
+	compare_last(cur_file, last_commit.filemd5, new_file, modified, copied, deleted);
+	
+	last_commit.commit_i++; 		fwrite(&last_commit.commit_i, sizeof(last_commit.commit_i),1,f); //commit_n
+	uint32_t tmp=cur_file.size();	fwrite(&tmp,sizeof(tmp),1,f); //file_n
+	tmp=new_file.size();			fwrite(&tmp,sizeof(tmp),1,f); //add_n
+	tmp=modified.size();			fwrite(&tmp,sizeof(tmp),1,f); //modified_n
+	tmp=copied.size();				fwrite(&tmp,sizeof(tmp),1,f); //copied_n
+	tmp=deleted.size();				fwrite(&tmp,sizeof(tmp),1,f); //deleted_n
+	
+	for(auto &item: new_file)
+	{
+		uint8_t len = item->name.length(); fwrite(&len,sizeof(len),1,f); 
+		fwrite(item->name.data(),sizeof(char), item->name.length(), f);
+	}
+	for(auto &item: modified)
+	{
+		uint8_t len = item->name.length(); fwrite(&len,sizeof(len),1,f); 
+		fwrite(item->name.data(),sizeof(char), item->name.length(), f);
+	}
+	for(auto &item: copied)
+	{
+		uint8_t len = item.first->name.length(); fwrite(&len,sizeof(len),1,f); 
+		fwrite(item.first->name.data(),sizeof(char), item.first->name.length(), f);
+
+		len = item.second->name.length(); fwrite(&len,sizeof(len),1,f); 
+		fwrite(item.second->name.data(),sizeof(char), item.second->name.length(), f);
+	}
+	for(auto &item: deleted)
+	{
+		uint8_t len = item->name.length(); fwrite(&len,sizeof(len),1,f); 
+		fwrite(item->name.data(),sizeof(char), item->name.length(), f);
 	}
 
-	read_int(log_f, timestamp);
+	//md5 part
+	for(auto &item: cur_file)
+	{
+		uint8_t len = item.name.length(); fwrite(&len,sizeof(len),1,f); 
+		fwrite(item.name.data(),sizeof(char),item.name.length(),f);
+		fwrite(item.md5,sizeof(char),MD5_DIGEST_LENGTH,f);
+	}
+
+	//time stamp
+	tmp = time(NULL);
+	fwrite(&tmp, sizeof(tmp), 1, f);
 }
