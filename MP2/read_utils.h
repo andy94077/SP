32d65a2b18f6e425e2ceb5929b9cb717a1d8d0f7
andy94077 <andy94077@gmail.com>
2018-11-27 00:30:13 +0800

    add read_int for ifstream

--- MP2/read_utils.h
+++ MP2/read_utils.h
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <fstream>
 template <typename T>
 inline void seek_and_read(FILE *f, int offset, int whence, T& buf)
 {
@@ -16,6 +17,18 @@ inline void read_int(FILE *f, T& first, Args&... args)
 {
 	fread(&first, sizeof(first), 1, f);
 	read_int(f, args...);
+}
+
+template <typename T>
+inline size_t read_int( ifstream& is, T& num)
+{
+	return is.read((char *)&num, sizeof(num));
+}
+template <typename T, typename... Args>
+inline void read_int(ifstream& is, T& first, Args&... args)
+{
+	is.read((char *)&first, sizeof(first));
+	read_int(is, args...);
 }
 
 inline void read_str(FILE *f,char *str,size_t n)
