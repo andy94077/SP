4a94b3ee747982f04ea6e1756c3b4d9c56efe2df
andy94077 <andy94077@gmail.com>
2018-12-05 15:32:15 +0800

    revise FileMd5

--- MP2/FileMd5.h
+++ MP2/FileMd5.h
@@ -7,50 +7,31 @@
 #include <openssl/md5.h>
 #include <cassert>
 #include <dirent.h>
+#include <linux/limits.h>
 using namespace std;
-void print_md5(unsigned char md5[16])
+inline void print_md5(unsigned char md5[16])
 {
 	for (int i = 0; i < MD5_DIGEST_LENGTH;i++)
 		printf("%02hhx", md5[i]);
 }
-void md5_to_string(string& md5, unsigned char c_md5[])
-{
-	char temp[20] = {0};
-	for (int i = 0; i < MD5_DIGEST_LENGTH; i++)
-		sprintf(temp,"%02hhx", c_md5[i]);
-	md5 = temp;
-}
+
+void md5_to_string(string &md5, unsigned char c_md5[]);
+
 class FileMd5
 {
   private:
-	void set_md5();
+	inline void set_md5();
 
   public:
 	string name, md5;
+	FileMd5() : name(""), md5(""){ }
 	FileMd5(const string& path) : name(path,path.find_last_of('/')+1) { set_md5();}
 	FileMd5(const char *__name, const string& __md5) : name(__name),md5(__md5) { }
 	FileMd5(const string& __name, const string& __md5) : name(__name),md5(__md5) { }
-	
-	bool operator<(const FileMd5 &a) { return name < a.name; }
+
+	bool operator<(const FileMd5 &a)const { return name < a.name; }
+	template <class T>
+	friend T &operator>>(T &stream, const FileMd5& fm5) { stream >> fm5.name >> fm5.md5; }
 };
-static char buf[1<<20];
-
-inline void FileMd5::set_md5()
-{
-	unsigned char __md5[MD5_DIGEST_LENGTH]={0};
-	memset(buf, 0, sizeof(buf));
-	FILE *f=fopen(name.data(), "rb");
-	assert(f!=NULL);
-
-	MD5_CTX context;
-	MD5_Init(&context);
-	size_t n;
-	while((n=fread(buf,sizeof(char),sizeof(buf),f))>0)
-		MD5_Update(&context,buf,n);
-	fclose(f);
-	MD5_Final(__md5,&context);
-
-	md5_to_string(md5,__md5);
-}
 
 #endif //_INCLUDED_FILEMD5_
