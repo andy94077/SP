b58be158c7f17a862c8490129be2acfab98c4a9b
andy94077 <andy94077@gmail.com>
2018-12-05 15:35:35 +0800

    read_utils now support streams

--- MP2/read_utils.h
+++ MP2/read_utils.h
@@ -1,7 +1,11 @@
+#ifndef __INCLUDED_READ_UTILS__
+#define __INCLUDED_READ_UTILS__
 #include <cstdio>
 #include <fstream>
+using namespace std;
+
 template <typename T>
-inline void seek_and_read(FILE *f, int offset, int whence, T& buf)
+inline void seek_and_read(FILE *f, int offset, int whence, T &buf)
 {
 	fseek(f, offset, whence);
 	fread(&buf, sizeof(buf), 1, f);
@@ -19,13 +23,13 @@ inline void read_int(FILE *f, T& first, Args&... args)
 	read_int(f, args...);
 }
 
-template <typename T>
-inline size_t read_int( ifstream& is, T& num)
+template <class Stream, typename T>
+inline size_t read_int( Stream& is, T& num)
 {
 	return is.read((char *)&num, sizeof(num));
 }
-template <typename T, typename... Args>
-inline void read_int(ifstream& is, T& first, Args&... args)
+template <class Stream, typename T, typename... Args>
+inline void read_int(Stream& is, T& first, Args&... args)
 {
 	is.read((char *)&first, sizeof(first));
 	read_int(is, args...);
@@ -36,3 +40,5 @@ inline void read_str(FILE *f,char *str,size_t n)
 	fread(str, sizeof(char), n, f);
 	str[n] = '\0';
 }
+
+#endif //__INCLUDED_READ_UTILS__
