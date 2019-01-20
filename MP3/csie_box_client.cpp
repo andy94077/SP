6830e3c67aa5a09fbd0a5833a3fcd931b2271423
andy94077 <andy94077@gmail.com>
2019-01-20 11:13:35 +0800

    [mp3]inotify for client.cpp

--- MP3/csie_box_client.cpp
+++ MP3/csie_box_client.cpp
@@ -1,5 +1,6 @@
 #include <sys/stat.h>
 #include <sys/select.h>
+#include <sys/inotify.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -16,21 +17,20 @@
 using namespace std;
 
 Config config;
-fs::path s2c_fifo, c2s_fifo, monitor_dir;
-fd_set read_set, write_set;
-int s2c, c2s;
+fd_set read_set;
+int s2c, c2s; //server_to_client, client_to_server
 
 void sig_pipe(int signo)
 {
-	//cout << "##get SIGPIPE\n";
-	//FD_CLR(c2s, &write_set);
+	cout << "##get SIGPIPE\n";
+	FD_CLR(s2c, &read_set);
 	return;
 }
 void sig_int(int signo)
 {
 	close(s2c);
 	close(c2s);
-	fs::remove_all(monitor_dir);
+	fs::remove_all(config.dir);
 	exit(0);
 }
 
@@ -44,17 +44,16 @@ int main(int argc, char const *argv[])
 
 	umask((__mode_t)0);
 
-	s2c_fifo = config.fifo_path / "server_to_client.fifo";
-	c2s_fifo = config.fifo_path / "client_to_server.fifo";
-
-	monitor_dir = config.dir;
 	fs::remove_all(config.dir);
 	mkdir(config.dir.c_str(), 0);
 
+	//connect to server
+	fs::path s2c_fifo = config.fifo_path / "server_to_client.fifo",	c2s_fifo = config.fifo_path / "client_to_server.fifo";
 	while(!fs::exists(s2c_fifo) || !fs::exists(c2s_fifo))	usleep(300000);
 	s2c = open(s2c_fifo.c_str(), O_RDONLY);
 	c2s = open(c2s_fifo.c_str(), O_WRONLY);
 
+	assert(s2c >= 0 && c2s >= 0);
 	//cout << "##s2c: " << s2c << " c2s: " << c2s << '\n';
 
 	//remove config.dir, and copy all contents from server
@@ -63,7 +62,39 @@ int main(int argc, char const *argv[])
 	fs::copy(config.fifo_path / "server", config.dir, fs::copy_options::overwrite_existing | fs::copy_options::recursive);
 	fs::permissions(config.dir, (fs::perms)0700);
 
-	while (1)
-		pause();
+
+	/*
+	*	prepare inotify
+	*/
+	int ifd = inotify_init();
+	assert(ifd >= 0);
+	inotify_add_watch(ifd, config.dir.c_str(),IN_CREATE | IN_DELETE | IN_MODIFY | IN_MOVE);
+
+	/*
+	*	prepare select
+	*/
+	FD_ZERO(&read_set);
+	FD_SET(s2c, &read_set);
+	FD_SET(ifd, &read_set);
+	int fd_n = max(s2c, ifd) + 1;
+
+	while (true)
+	{
+		timeval tm = {0, 300000}; //0.3 seconds
+		fd_set work_set = read_set;
+		select(fd_n, &work_set, NULL, NULL, &tm);
+		
+		if(FD_ISSET(ifd,&work_set))
+		{
+			//avoid copying files synced from server
+			//copy files to fifo_path/client
+			//notify server
+		}
+
+		if(FD_ISSET(s2c,&work_set))
+		{
+			//move files from fifo_path/server
+		}
+	}
 	return 0;
 }
