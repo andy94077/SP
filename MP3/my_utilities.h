1f93f282dd267aaf02c20ba98e76a061725c8af9
andy94077 <andy94077@gmail.com>
2019-01-22 21:05:06 +0800

    [mp3]add operator-(path,path) in my_utilities.h

--- MP3/my_utilities.h
+++ MP3/my_utilities.h
@@ -1,12 +1,15 @@
 #ifndef __INCLUDE_MY_UTILITIES__
 #define __INCLUDE_MY_UTILITIES__
-
+#include "filesystem_utilities.h"
 #define CHECK_MASK_AND_PRINT(mask, option) \
 	{                                      \
 		if ((mask) & (option))             \
 			cout << #option << ' ';        \
 	}
 
-
+fs::path operator -(const fs::path& p1,const fs::path& p2)
+{
+	return fs::path(p1.string().substr(p2.string().length() + 1));//+1 means the '/' symbol
+}
 
 #endif //__INCLUDE_MY_UTILITIES__
